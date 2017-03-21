7336ab9dd60d7bdae94041a608132b5f91196e90
Zachary Burnett <zrb@umd.edu>
2017-03-20 21:36:42 -0400

    camera code

--- virde.py
+++ virde.py
@@ -1,21 +1,40 @@
 # modified from https://www.raspberrypi.org/learning/sense-hat-data-logger/code/Sense_Logger_v4.py
 
 # imports
-from time import time
 from datetime import datetime
 from threading import Thread
-from time import sleep
+from time import sleep, time
 
 from sense_hat import SenseHat
+from picamera import PiCamera
 
-# define logging interval in seconds
-logging_interval = 1
+# instantiate hardware objects
+sensehat = SenseHat()
+camera = PiCamera()
 
-# define name of logfile
-filename = "log"
+# define logging intervals in seconds
+sensehat_logging_interval = 1
+picamera_logging_interval = 6
 
-# append datetime to filename
-filename = filename + "_" + str(int(time.time())) + ".csv"
+# define path to log directory
+logdir = '~/Desktop/virde_log/'
+
+# append datetime to filename and open logfile as append connection
+logfile = open(logdir + 'log' + '_' + str(int(time.time())) + '.csv', 'a')
+
+# construct logfile header
+header = ['datetime']
+header.append('temp_h')
+header.append('temp_p')
+header.append('humidity')
+header.append('pressure')
+header.extend(['pitch', 'roll', 'yaw'])
+header.extend(['mag_x', 'mag_y', 'mag_z'])
+header.extend(['accel_x', 'accel_y', 'accel_z'])
+header.extend(['gyro_x', 'gyro_y', 'gyro_z'])
+
+# write header to logfile
+logfile.write(','.join(str(value) for value in header) + '\n')
 
 # define function to return a csv line of all sensehat data
 def get_sensehat_csv_line(sensehat):
@@ -27,40 +46,39 @@ def get_sensehat_csv_line(sensehat):
     output_data.append(sensehat.get_pressure())
 
     orientation = sensehat.get_orientation()
-    output_data.extend([orientation["yaw"], orientation["pitch"], orientation["roll"]])
+    output_data.extend([orientation['yaw'], orientation['pitch'], orientation['roll']])
 
     magnetometer = sensehat.get_compass_raw()
-    output_data.extend([magnetometer["x"], magnetometer["y"], magnetometer["z"]])
+    output_data.extend([magnetometer['x'], magnetometer['y'], magnetometer['z']])
 
     accelerometer = sensehat.get_accelerometer_raw()
-    output_data.extend([accelerometer["x"], accelerometer["y"], accelerometer["z"]])
+    output_data.extend([accelerometer['x'], accelerometer['y'], accelerometer['z']])
 
     gyroscope = sensehat.get_gyroscope_raw()
-    output_data.extend([gyroscope["x"], gyroscope["y"], gyroscope["z"]])
+    output_data.extend([gyroscope['x'], gyroscope['y'], gyroscope['z']])
 
     # return output data in comma separated form
-    return ",".join(str(value) for value in output_data)
+    return ','.join(str(value) for value in output_data)
 
-# construct logfile header
-header = ["datetime"]
-header.append("temp_h")
-header.append("temp_p")
-header.append("humidity")
-header.append("pressure")
-header.extend(["pitch", "roll", "yaw"])
-header.extend(["mag_x", "mag_y", "mag_z"])
-header.extend(["accel_x", "accel_y", "accel_z"])
-header.extend(["gyro_x", "gyro_y", "gyro_z"])
+def sensehat_logging_thread(logfile):
+    while True:
+        logfile.write(get_sensehat_csv_line(sensehat) + '\n')
+        sleep(sensehat_logging_interval)
+      
+def picamera_logging_thread():
+    while True:      
+        # let automatic exposure settle
+        camera.start_preview()
+        sleep(3)
+        
+        # capture in PNG format at native resolution
+        camera.capture(logdir + 'image' + '_' + str(int(time.time())) + '.png')
+        
+        # capture in unencoded RGB format
+        camera.capture(logdir + 'image' + '_' + str(int(time.time())) + '.data', 'rgb')
+        
+        sleep(picamera_logging_interval)
 
-# instantiate sensehat object
-sensehat = SenseHat()
-
-# open logfile
-with open(filename, "a") as logfile:
-    # write header to logfile
-    logfile.write(",".join(str(value) for value in header) + "\n")
-    
-    # main loop
-    while True:     
-        logfile.write(+"\n")
-        sleep(logging_interval)
+# start logging threads
+Thread(target = sensehat_logging_thread).start()
+Thread(target = picamera_logging_thread).start()
