2f31b3e5c7a7fb8fd8fcade00abae5e4817ba347
Zachary Burnett <zrb@umd.edu>
2017-03-21 14:14:57 -0400

    added shutter button for testing

--- virde.py
+++ virde.py
@@ -6,11 +6,14 @@ from datetime import datetime
 from threading import Thread
 from time import sleep, time
 
+import pygame
+from pygame.locals import *
 from sense_hat import SenseHat
 from picamera import PiCamera
 
-# instantiate hardware objects
+# instantiate sensehat and pygame
 sensehat = SenseHat()
+pygame.init()
 
 # define logging intervals in seconds
 sensehat_logging_interval = 1
@@ -74,6 +77,19 @@ def get_sensehat_csv_line(sensehat):
     # return output data in comma separated form
     return ','.join(str(value) for value in output_data)
 
+def picamera_capture():
+    with PiCamera(sensor_mode = 2) as camera:
+        camera.resolution = (3280, 2464)
+    
+        # let automatic exposure settle
+        sleep(1)
+
+        # capture in PNG format at native resolution
+        camera.capture(os.path.join(image_dir, 'image_' + str(int(time())) + '.png'))
+        
+        # capture in unencoded RGB format
+        camera.capture(os.path.join(image_dir, 'image_' + str(int(time())) + '.data'), 'rgb')
+
 def sensehat_logging_thread():
     while time() < start_time + timeout:
         sensor_log_file.write(get_sensehat_csv_line(sensehat) + '\n')
@@ -81,21 +97,24 @@ def sensehat_logging_thread():
       
 def picamera_logging_thread():
     while time() < start_time + timeout:
-        with PiCamera(sensor_mode = 2) as camera:
-            camera.resolution = (3280, 2464)
-        
-            # let automatic exposure settle
-            sleep(1)
-
-            # capture in PNG format at native resolution
-            camera.capture(os.path.join(image_dir, 'image_' + str(int(time())) + '.png'))
-            
-            # capture in unencoded RGB format
-            camera.capture(os.path.join(image_dir, 'image_' + str(int(time())) + '.data'), 'rgb')
+        # capture image
+        picamera_capture()
 
         # delay the specified interval
         sleep(picamera_logging_interval - 1)
 
 # start logging threads
-Thread(target = sensehat_logging_thread).start()
-Thread(target = picamera_logging_thread).start()
+#Thread(target = sensehat_logging_thread).start()
+#Thread(target = picamera_logging_thread).start()
+
+while time() < start_time + timeout:
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_RETURN:
+                for second in range(10, 0):
+                    sensehat.show_letter(second)
+                    sensor_log_file.write(get_sensehat_csv_line(sensehat) + '\n')
+                    sleep(1)
+                picamera_capture()
+                sensehat.clear()
+                
