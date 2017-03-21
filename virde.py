ef3a4f76cf79388dcce971da2ddec9c653a7fdd5
Zach Burnett <zrb@umd.edu>
2017-03-21 13:39:56 -0400

    update

--- virde.py
+++ virde.py
@@ -11,25 +11,32 @@ from picamera import PiCamera
 
 # instantiate hardware objects
 sensehat = SenseHat()
-camera = PiCamera(sensor_mode = 2)
-
-timeout = 12   # [seconds]
-timeout_start = time()
 
 # define logging intervals in seconds
 sensehat_logging_interval = 1
 picamera_logging_interval = 6
 
+timeout = picamera_logging_interval * 3
+start_time = time()
+
 # define path to log directory
-logdir = '/home/pi/Desktop/virde_log/'
+log_dir = '/home/pi/Desktop/virde_log/'
 
-if not os.path.exists(logdir):
-    os.mkdir(logdir)
+# create path to log directory if it doesn't exist
+if not os.path.exists(log_dir):
+    os.mkdir(log_dir)
 
-# append datetime to filename and open logfile as append connection
-logfile = open(os.path.join(logdir, 'log' + '_' + str(int(time())) + '.csv'), 'a')
+# define image directory
+image_dir = os.path.join(log_dir, 'images_' + str(int(start_time)))
 
-# construct logfile header
+# create image directory if it doesn't exist
+if not os.path.exists(image_dir):
+    os.mkdir(image_dir)
+            
+# append datetime to filename and open logfile with append connection
+sensor_log_file = open(os.path.join(log_dir, 'sensor_log_' + str(int(start_time)) + '.csv'), 'a')
+
+# construct CSV header
 header = ['datetime']
 header.append('temp_h')
 header.append('temp_p')
@@ -40,8 +47,8 @@ header.extend(['mag_x', 'mag_y', 'mag_z'])
 header.extend(['accel_x', 'accel_y', 'accel_z'])
 header.extend(['gyro_x', 'gyro_y', 'gyro_z'])
 
-# write header to logfile
-logfile.write(','.join(str(value) for value in header) + '\n')
+# write header to file
+sensor_log_file.write(','.join(str(value) for value in header) + '\n')
 
 # define function to return a csv line of all sensehat data
 def get_sensehat_csv_line(sensehat):
@@ -67,24 +74,27 @@ def get_sensehat_csv_line(sensehat):
     # return output data in comma separated form
     return ','.join(str(value) for value in output_data)
 
-def sensehat_logging_thread(logfile):
-    while time() < timeout_start + timeout:
-        logfile.write(get_sensehat_csv_line(sensehat) + '\n')
+def sensehat_logging_thread():
+    while time() < start_time + timeout:
+        sensor_log_file.write(get_sensehat_csv_line(sensehat) + '\n')
         sleep(sensehat_logging_interval)
       
 def picamera_logging_thread():
-    while time() < timeout_start + timeout:
-        # let automatic exposure settle
-        camera.start_preview()
-        sleep(3)
+    while time() < start_time + timeout:
+        with PiCamera(sensor_mode = 2) as camera:
+            camera.resolution = (3280, 2464)
         
-        # capture in PNG format at native resolution
-        camera.capture(logdir + 'image' + '_' + str(int(time())) + '.png')
-        
-        # capture in unencoded RGB format
-        camera.capture(logdir + 'image' + '_' + str(int(time())) + '.data', 'rgb')
-        
-        sleep(picamera_logging_interval - 3)
+            # let automatic exposure settle
+            sleep(1)
+
+            # capture in PNG format at native resolution
+            camera.capture(os.path.join(image_dir, 'image_' + str(int(time())) + '.png'))
+            
+            # capture in unencoded RGB format
+            camera.capture(os.path.join(image_dir, 'image_' + str(int(time())) + '.data'), 'rgb')
+
+        # delay the specified interval
+        sleep(picamera_logging_interval - 1)
 
 # start logging threads
 Thread(target = sensehat_logging_thread).start()
