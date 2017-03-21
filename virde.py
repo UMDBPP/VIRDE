58aea68d9910fd391be215643adbd055d43bf6d3
Zachary Burnett <zrb@umd.edu>
2017-03-20 20:52:51 -0400

    finished sensehat logging

--- virde.py
+++ virde.py
@@ -1,56 +1,66 @@
-# adapted from
-# https://www.raspberrypi.org/learning/sense-hat-data-logger/worksheet/
+# modified from https://www.raspberrypi.org/learning/sense-hat-data-logger/code/Sense_Logger_v4.py
 
 # imports
-from sense_hat import SenseHat
+from time import time
 from datetime import datetime
-from time import sleep
 from threading import Thread
+from time import sleep
 
-# definitions
-FILENAME = ""
-DELAY = 1
+from sense_hat import SenseHat
 
-# functions
-def get_sense_data():
-    sense_data=[]
+# define logging interval in seconds
+logging_interval = 1
 
-    sense_data.append(sense.get_temperature_from_humidity())
-    sense_data.append(sense.get_temperature_from_pressure())
-    sense_data.append(sense.get_humidity())
-    sense_data.append(sense.get_pressure())
+# define name of logfile
+filename = "log"
 
-    o = sense.get_orientation()
-    yaw = o["yaw"]
-    pitch = o["pitch"]
-    roll = o["roll"]
-    sense_data.extend([pitch,roll,yaw])
+# append datetime to filename
+filename = filename + "_" + str(int(time.time())) + ".csv"
 
-    mag = sense.get_compass_raw()
-    mag_x = mag["x"]
-    mag_y = mag["y"]
-    mag_z = mag["z"]
-    sense_data.extend([mag_x,mag_y,mag_z])
+# define function to return a csv line of all sensehat data
+def get_sensehat_csv_line(sensehat):
+    output_data = [datetime.now()]
 
-    acc = sense.get_accelerometer_raw()
-    x = acc["x"]
-    y = acc["y"]
-    z = acc["z"]
-    sense_data.extend([x,y,z])
+    output_data.append(sensehat.get_temperature_from_humidity())
+    output_data.append(sensehat.get_temperature_from_pressure())
+    output_data.append(sensehat.get_humidity())
+    output_data.append(sensehat.get_pressure())
 
-    gyro = sense.get_gyroscope_raw()
-    gyro_x = gyro["x"]
-    gyro_y = gyro["y"]
-    gyro_z = gyro["z"]
-    sense_data.extend([gyro_x,gyro_y,gyro_z])
+    orientation = sensehat.get_orientation()
+    output_data.extend([orientation["yaw"], orientation["pitch"], orientation["roll"]])
 
-    sense_data.append(datetime.now())
+    magnetometer = sensehat.get_compass_raw()
+    output_data.extend([magnetometer["x"], magnetometer["y"], magnetometer["z"]])
 
-    return sense_data
+    accelerometer = sensehat.get_accelerometer_raw()
+    output_data.extend([accelerometer["x"], accelerometer["y"], accelerometer["z"]])
 
-# main
-sense = SenseHat()
+    gyroscope = sensehat.get_gyroscope_raw()
+    output_data.extend([gyroscope["x"], gyroscope["y"], gyroscope["z"]])
 
-while True:
-  sense_data = get_sense_data()
-  print(sense_data)
+    # return output data in comma separated form
+    return ",".join(str(value) for value in output_data)
+
+# construct logfile header
+header = ["datetime"]
+header.append("temp_h")
+header.append("temp_p")
+header.append("humidity")
+header.append("pressure")
+header.extend(["pitch", "roll", "yaw"])
+header.extend(["mag_x", "mag_y", "mag_z"])
+header.extend(["accel_x", "accel_y", "accel_z"])
+header.extend(["gyro_x", "gyro_y", "gyro_z"])
+
+# instantiate sensehat object
+sensehat = SenseHat()
+
+# open logfile
+with open(filename, "a") as logfile:
+    # write header to logfile
+    logfile.write(",".join(str(value) for value in header) + "\n")
+    
+    # main loop
+    while True:     
+        logfile.write(+"\n")
+        sleep(logging_interval)
